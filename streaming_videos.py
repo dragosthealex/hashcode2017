0b32005d2d617dc14c7c1ab2aa00d302b5b181a3
Alex Radu <dragosthealx@gmail.com>
2017-02-23 20:35:13 +0000

    Implemented knapsack for single cache

--- streaming_videos.py
+++ streaming_videos.py
@@ -1,5 +1,7 @@
 import sys
 
+CACHE_SIZE = 0
+
 
 class Video:
 
@@ -46,6 +48,33 @@ class Cache:
     self._endpoints = []
     self._videos = []
 
+  def get_optimal_videos(self, video_weights_matrix):
+    no_videos = len(video_weights_matrix)
+    valueMatrix = [[] for _ in no_videos]
+    valueMatrix[0] = [0 for _ in no_videos]
+    keep = [[0 for _ in CACHE_SIZE] for _ in no_videos]
+    for i, video in enumerate(video_weights_matrix):
+      for j in range(CACHE_SIZE):
+        # If video's weight less than current weight limit
+        # we either have the same value as when we could use prev videos,
+        # or we add the video, but subtract its weight
+        if(video[0] <= j):
+          if valueMatrix[i - 1][j] <= video[1] + \
+             valueMatrix[i - 1][j - video[0]]:
+            valueMatrix[i][j] = video[1] + valueMatrix[i - 1][j - video[0]]
+            keep[i][j] = 1
+          else:
+            valueMatrix[i][j] = valueMatrix[i - 1][j]
+        else:
+          valueMatrix[i][j] = valueMatrix[i - 1][j]
+    videos = []
+    k = CACHE_SIZE
+    for i in range(no_videos, 0, -1):
+      if keep[i][k] == 1:
+        videos.append(video_weights_matrix[i])
+        k -= video_weights_matrix[i][1]
+    return videos
+
   def __str__(self):
     return ("id: {} endpoints: {} videos: " +
             "{} latencies: {}").format(self._id,
@@ -67,6 +96,7 @@ if __name__ == '__main__':
   no_endpoints = int(line[1])
   no_requests = int(line[2])
   no_caches = int(line[3])
+  CACHE_SIZE = int(line[4])
   # Read endpoints and latencies
   endpoints = []
   # Assign caches
